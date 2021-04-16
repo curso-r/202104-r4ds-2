8266181e748e83b8e515d8aa9be7852a9f510e53
C. Lente <7017340+clente@users.noreply.github.com>
2021-04-15 21:39:12 -0300

    Anotações durante a aula

--- scripts/05-lubridate.R
+++ scripts/05-lubridate.R
@@ -35,6 +35,20 @@ t1 - t2
 # Lista dos fusos
 OlsonNames()
 
+# Output
+format.Date(now(), "%d/%m/%Y %H:%M:%S")
+
+# Conversão para SP (com horário de verão!)
+with_tz(dmy_hms("15/02/2019 02:25:00"), tzone = "America/Sao_Paulo")
+with_tz(dmy_hms("15/02/2020 02:25:00"), tzone = "America/Sao_Paulo")
+
+# Arrendondar
+floor_date(today(), unit = "month") # Função chão
+rollforward(today())                # Fim do mês
+
+# Feriados (usar o pacote bizdays)
+bizdays::add.bizdays("2020-12-30", 5, cal = "Brazil/ANBIMA")
+
 # Extra: clock ------------------------------------------------------------
 
 # Biblioteca fresquinha!
